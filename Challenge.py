7b85184837c9033c5c731c7562047c53791514d1
Ameera <nurameera1802@gmail.com>
2021-07-24 13:17:00 +0800

    Initial commit

---
+++ Challenge.py
@@ -0,0 +1,12 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+markList=[45,60,70,40,80,90,55,75,75,20,65]
+def testMark
